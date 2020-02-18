5c5f29b5a6299a30f58c0b4ba970e75377b50971
MantasKrik <47982279+MantasKrik@users.noreply.github.com>
2020-02-18 20:48:34 +0200

    Created a character controller

---
+++ Assets/Mantas/Scripts/MouseLook.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MouseLook : MonoBehaviour
+{
+
+    public Camera camera;
+    public Transform player;
+    public float rotationSpeed;
+
+    private float mouseX;
+    private float mouseY;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Cursor.lockState = CursorLockMode.Locked;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        mouseX = Input.GetAxis("Mouse X");
+        mouseY = Input.GetAxis("Mouse Y");
+
+        player.Rotate(new Vector3(0, player.rotation.x + mouseX * rotationSpeed * Time.deltaTime, 0));
+
+        camera.transform.eulerAngles -= new Vector3(mouseY, 0, 0);
+
+    }
+}
---
+++ Assets/Mantas/Scripts/PlayerController.cs
@@ -0,0 +1,74 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    private Rigidbody rb;
+
+    [Header("Movement")]
+    public float movementSpeed = 10f;
+    public float maxSpeed = 5f;
+
+    public Transform groundCheck;
+    public float jumpForce = 4f;
+    public bool isGrounded;
+    public bool shouldJump;
+
+    private Vector3 inputMovement;
+   
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        GetMovementInput();
+        CheckGround();
+        
+    }
+
+    private void FixedUpdate()
+    {
+        Move();
+
+        if (isGrounded && shouldJump)
+        {
+            Debug.Log("Jump");
+            rb.AddRelativeForce(Vector3.up * jumpForce * rb.mass, ForceMode.Impulse);
+            shouldJump = false;
+        }
+            
+    }
+
+
+    private void CheckGround()
+    {
+        
+    }
+
+    private void Move()
+    {
+        if (!(rb.velocity.sqrMagnitude > maxSpeed))
+        {
+            rb.AddRelativeForce(inputMovement * movementSpeed * rb.mass * Time.fixedTime, ForceMode.Acceleration);
+        }
+    }
+
+    private void GetMovementInput()
+    {
+        inputMovement.x = Input.GetAxisRaw("Horizontal");
+        inputMovement.z = Input.GetAxisRaw("Vertical");
+
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            shouldJump = true;
+        }
+    }
+
+}
